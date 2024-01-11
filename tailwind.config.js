b0af5a2308be194da8e28af67236f9b7fd6ea0ed
Bojan Ilic <ilic.bojan.bi@gmail.com>
2024-01-11 21:23:02 +0100

    style: Update primary color in Tailwind CSS configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
     theme: {
         extend: {
             colors: {
-                primary: '#7DAE5F',
+                primary: '#51cf66',
             },
         },
     },
