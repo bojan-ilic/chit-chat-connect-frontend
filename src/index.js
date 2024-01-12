5ccd64e32e3f9dbe6b98956cbaa50146958ff256
Bojan Ilic <ilic.bojan.bi@gmail.com>
2024-01-12 15:51:51 +0100

    feat: Wrap the entire application with Redux Provider for global state management using the 'store' configuration in entry point index.js file

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import React from 'react';
 // Import 'ReactDOM' submodule from the 'react-dom' library
 import ReactDOM from 'react-dom/client';
 
+// Import global styles
 import './index.scss';
 
 // Import the main application component
@@ -15,11 +16,26 @@ import {
     RouterProvider, // Component that provides routing context to its descendants
 } from 'react-router-dom';
 
+// Imports the 'Provider' component from 'react-redux' to globally provide access to the Redux store for all components
+import { Provider } from 'react-redux';
+
+// Import the Redux store configuration
+import store from './store/store';
+
 // Import page components for application routes
 import Home from './pages/Home/Home.jsx'; // Main landing page
 import Register from './pages/Register/Register'; // Import Register page component
 import Login from './pages/Login/Login'; // Import Login page component
 import Error from './pages/Error/Error'; // Import Error page component
+
+/**
+ * @file Entry point for the Chit-Chat-Connect React application.
+ *
+ * This file coordinates the rendering of the application, handles routing, and manages the global state with Redux.
+ * It imports necessary libraries, components, and configurations, including React, ReactDOM, React Router, and Redux.
+ * The application routes are defined, and the Redux store is connected globally using the 'Provider' component.
+ * Renders the main application within a 'RouterProvider' for efficient route handling.
+ */
 
 // Define routes for the application
 const router = createBrowserRouter([
@@ -51,7 +67,10 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     // Wrapping with StrictMode for additional development checks
     <React.StrictMode>
-        {/* Provide the router configuration to the RouterProvider */}
-        <RouterProvider router={router} />
+        {/* Wrap the entire app with Redux Provider, granting global access to the application state for all components. */}
+        <Provider store={store}>
+            {/* Provide the router configuration to the RouterProvider */}
+            <RouterProvider router={router} />
+        </Provider>
     </React.StrictMode>,
 );
