709aa856d56dca9c1378d082cce50e2c837e44b5
Bojan Ilic <ilic.bojan.bi@gmail.com>
2024-03-04 18:52:47 +0100

    feat: Add 'FooterLogoBox' component for homepage navigation with footer logo

--- src/components/FooterLogoBox/FooterLogoBox.jsx
+++ src/components/FooterLogoBox/FooterLogoBox.jsx
@@ -1,6 +1,18 @@
-// Import the site logo file
+// Import the footer logo image from the assets folder
 import Logo from '../../assets/ChitChatConnect-Logo-Footer.svg';
+
+// Import Link from React Router for route-based navigation without full page reloads
 import { Link } from 'react-router-dom';
+
+/**
+ * FooterLogoBox component
+ *
+ * This component is responsible for displaying the logo within the footer of the application.
+ * When clicked, navigates the user back to the homepage.
+ *
+ * @returns {React.Component} A div containing the logo image wrapped in a `Link` component, which redirects to the homepage upon click.
+ * @type {React.FC}
+ */
 const FooterLogoBox = () => {
     return (
         <div>
@@ -11,4 +23,5 @@ const FooterLogoBox = () => {
     );
 };
 
+// Export FooterLogoBox for accessibility throughout the application
 export default FooterLogoBox;
