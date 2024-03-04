bc1483b08536ab49b0f8e7fbc30ab5847eac04ec
Bojan Ilic <ilic.bojan.bi@gmail.com>
2024-03-04 19:50:36 +0100

    docs: Enhance Footer with doc comments and integrate FooterLogoBox for logo navigation

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,24 @@
-// FooterNavBar component for footer links
+// Import FooterNavBar component for footer links
 import FooterNavBar from '../FooterNavBar/FooterNavBar';
 
-// FooterContact component for contact information
+// Import FooterContact component for contact information
 import FooterContact from '../FooterContact/FooterContact';
 
+// Import the FooterLogoBox component for displaying the footer logo with navigation functionality
+import FooterLogoBox from '../FooterLogoBox/FooterLogoBox';
+
+/**
+ * Footer component
+ *
+ * This component serves as the global footer for the application.
+ * Displays three key components:
+ * - FooterNavBar for navigation links,
+ * - FooterLogoBox for branding with a navigational logo,
+ * - FooterContact for contact information.
+ *
+ * @returns {React.Component} A footer element containing navigation links, the application's logo, and contact information, structured within a responsive layout.
+ * @type {React.FC}
+ */
 const Footer = () => {
     return (
         <footer className="border-primary mt-20 rounded-lg border bg-white p-4">
@@ -11,6 +26,8 @@ const Footer = () => {
                 <div className="flex items-center justify-between">
                     {/* FooterNavBar component displays footer links */}
                     <FooterNavBar />
+                    {/* FooterLogoBox component renders the logo in the footer that redirects to the homepage when clicked */}
+                    <FooterLogoBox />
                     {/* FooterContact component displays contact information */}
                     <FooterContact />
                 </div>
@@ -19,4 +36,5 @@ const Footer = () => {
     );
 };
 
+// Export Footer for accessibility throughout the application
 export default Footer;
