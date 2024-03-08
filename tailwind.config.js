b396c2de6f999fb1f32f6b1792fdcf97685e4189
Bojan Ilic <ilic.bojan.bi@gmail.com>
2024-03-08 19:39:23 +0100

    style: Add warning and accent colors to 'tailwind.config.js' for UI enhancement

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,8 @@ module.exports = {
             colors: {
                 primary: '#51cf66',
                 secondary: '#000000',
+                warning: '#FF7800',
+                accent: '#333333',
             },
             fontFamily: {
                 primary: ['"Open Sans"', 'sans-serif'],
