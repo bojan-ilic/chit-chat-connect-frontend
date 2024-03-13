1414b16889c8d13443e57694239cfc6c739bdac2
Bojan Ilic <ilic.bojan.bi@gmail.com>
2024-03-13 19:17:45 +0100

    feat: Introduce 'HeaderLogoBox' for easy navigation back to the home page through the logo

--- src/components/HeaderLogoBox/HeaderLogoBox.jsx
+++ src/components/HeaderLogoBox/HeaderLogoBox.jsx
@@ -4,12 +4,22 @@ import Logo from '../../assets/ChitChatConnect-Logo.svg';
 // Import Link from React Router for route-based navigation without full page reloads
 import { Link } from 'react-router-dom';
 
-const LogoBox = () => {
+/**
+ * HeaderLogoBox.jsx
+ *
+ * This component renders the logo box in the header of the application.
+ * It displays the application's logo and links back to the home page when clicked.
+ *
+ * @returns {JSX.Element} A Link component wrapping the logo, ensuring the user is navigated to the home page on click.
+ */
+const HeaderLogoBox = () => {
     return (
+        // Link to the home page with the application's logo
         <Link to="/" className="text-3xl font-bold leading-none">
             <img src={Logo} alt="Logo" />
         </Link>
     );
 };
 
-export default LogoBox;
+// Export HeaderLogoBox component for accessibility throughout the application
+export default HeaderLogoBox;
