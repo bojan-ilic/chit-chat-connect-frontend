db2d154fbb0c9391e06a351974b9c7ea939da190
Bojan Ilic <ilic.bojan.bi@gmail.com>
2024-03-13 19:24:03 +0100

    feat: Implement dynamic header with logo, navigation links, and user account access

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,5 @@
-// Import LogoBox component responsible for displaying the site logo
-import LogoBox from '../LogoBox/LogoBox';
+// Import HeaderLogoBox component responsible for displaying the site logo
+import HeaderLogoBox from '../HeaderLogoBox/HeaderLogoBox';
 
 // Import HeaderNavBar component responsible for rendering the navigation bar
 import HeaderNavBar from '../HeaderNavBar/HeaderNavBar';
@@ -7,19 +7,37 @@ import HeaderNavBar from '../HeaderNavBar/HeaderNavBar';
 // Import HeaderAccount component responsible for managing account-related elements
 import HeaderAccount from '../HeaderAccount/HeaderAccount';
 
+// Import useSelector hook from Redux to access the application state
+import { useSelector } from 'react-redux';
+
+/**
+ * Header.jsx
+ *
+ * This component renders the application's header, including the logo, navigation bar, and user account section.
+ * It dynamically adjusts to display the user account section based on the login state.
+ *
+ * @returns {JSX.Element} A header element containing the logo, navigation links, and, if logged in, the user account section.
+ */
 const Header = () => {
+    // Access 'user' and 'loginSuccess' state from the Redux store to determine if the user is logged in
+    const { user, loginSuccess } = useSelector((state) => state.userStore);
+    // Determine if the user is logged in based on 'loginSuccess' and presence of 'user' data
+    const isLoggedIn = loginSuccess && Object.keys(user).length > 0;
+
     return (
+        // Header container with conditional rendering for the account section
         <header className="border-primary relative flex items-center justify-between rounded-lg border bg-white px-4 py-4">
-            {/* LogoBox component renders the site's logo */}
-            <LogoBox />
+            {/* 'HeaderLogoBox' component renders the site's logo */}
+            <HeaderLogoBox />
+            {/* Render the navigation bar, passing in login state */}
             <div className="mx-4 flex flex-grow items-center justify-center">
-                {/* HeaderNavBar component renders the navigation bar */}
-                <HeaderNavBar />
+                <HeaderNavBar isLoggedIn={isLoggedIn} />
             </div>
-            {/* HeaderAccount component manages account-related elements */}
-            <HeaderAccount />
+            {/* Conditionally render the user account section if logged in */}
+            {isLoggedIn && <HeaderAccount />}
         </header>
     );
 };
 
+// Export 'Header' component for accessibility throughout the application
 export default Header;
