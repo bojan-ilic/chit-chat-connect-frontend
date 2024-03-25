8fafe2692788db09b70d8c99231e8c2c329cea5a
Bojan Ilic <ilic.bojan.bi@gmail.com>
2024-03-25 11:30:32 +0100

    refactor: Update 'FooterContact.jsx' to include social media links and contact icons, enhancing user connectivity options

--- src/components/FooterContact/FooterContact.jsx
+++ src/components/FooterContact/FooterContact.jsx
@@ -1,22 +1,52 @@
+import { FaInstagram } from 'react-icons/fa';
+import { SlSocialFacebook } from 'react-icons/sl';
+import { GrLocation } from 'react-icons/gr';
+import { MdOutlineEmail } from 'react-icons/md';
+import { BsTelephone } from 'react-icons/bs';
+import { Link } from 'react-router-dom';
+
 const FooterContact = () => {
     return (
         <div className="footer-contact">
-            <p>
-                Address:
-                <a
-                    href="https://www.google.com/maps?q=Knez+Mihaila+20,+Belgrade,+Serbia"
+            <h3 className="mb-2.5 font-bold uppercase">Connect with us</h3>
+            <div className="flex flex-row justify-center gap-2">
+                <Link
+                    to="https://instagram.com"
                     target="_blank"
                     rel="noopener noreferrer"
+                    className="text-primary hover:text-accent"
                 >
-                    123 Street, City, Country
-                </a>
-            </p>
-            <p>
-                Email: <a href="mailto:info@example.com">info@example.com</a>
-            </p>
-            <p>
-                Phone: <a href="tel:+1234567890">+1234567890</a>
-            </p>
+                    <FaInstagram size={32} />
+                </Link>
+                <Link
+                    to="https://facebook.com"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="text-primary hover:text-accent"
+                >
+                    <SlSocialFacebook size={32} />
+                </Link>
+                <Link
+                    to="https://www.google.com/maps?q=Knez+Mihaila+20,+Belgrade,+Serbia"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="text-primary hover:text-accent"
+                >
+                    <GrLocation size={32} />
+                </Link>
+                <Link
+                    to="mailto:info@example.com"
+                    className="text-primary hover:text-accent"
+                >
+                    <MdOutlineEmail size={32} />
+                </Link>
+                <Link
+                    to="tel:+1234567890"
+                    className="text-primary hover:text-accent"
+                >
+                    <BsTelephone size={32} />
+                </Link>
+            </div>
         </div>
     );
 };
